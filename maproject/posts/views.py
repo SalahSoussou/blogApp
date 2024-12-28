10270648e21a3fd15f067c6e8bee7821ee4514ef
SalahSoussou <soussousalahiddine.com>
2024-12-28 19:27:44 +0100

    Sort posts by creation date in posts list view

--- maproject/posts/views.py
+++ maproject/posts/views.py
@@ -3,7 +3,7 @@ from .models import Post
 # Create your views here.
 
 def posts_lest(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_at')
     return render(request, 'posts/posts_lest.html', {'posts': posts})
 
 
